b2f9f87ed101673d66bc7c24439bedea0dcc7244
huac611 <117928751+huac611@users.noreply.github.com>
2022-11-11 02:32:49 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'nlJyZ4wPFQMtlMXvxFK1pMwIxamWLmpXTGOlVd1RPf0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'fW3tfKZXWx4t5C39SlLl0hhySNDfqi84AEcDDthxX8I',
+      useTemplateId: 'MZ4woiDkTSctxukSayD0_J6ISkSSGtaVgUMs9H_WgTM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-17',
       festivals: [

5dc0bb63e543bcb09c8ce823888475d5061a02e9
huac611 <117928751+huac611@users.noreply.github.com>
2022-11-11 08:09:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '椰椰宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'nlJyZ4wPFQMtlMXvxFK1pMwIxamWLmpXTGOlVd1RPf0',
+      id: 'otG_X5g4PKmHNr-1EXbHbt8Y0DPU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'MZ4woiDkTSctxukSayD0_J6ISkSSGtaVgUMs9H_WgTM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
